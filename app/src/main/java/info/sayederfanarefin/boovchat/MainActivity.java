8239596ff632823edc5be145b410f5b77cb8ab9d
Sayed Erfan Arefin <SayedErfan.Arefin@studentpartner.com>
2015-08-06 07:22:50 -0700

    trying again 2c
    after 2b probably some files were not synced, so going for the commit
    again :p

--- app/src/main/java/info/sayederfanarefin/boovchat/MainActivity.java
+++ app/src/main/java/info/sayederfanarefin/boovchat/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
         buttonSignup = (Button) findViewById(R.id.button_sign_up);
         editTextEmail = (EditText) findViewById(R.id.editText_user_email);
         editTextPassword = (EditText) findViewById(R.id.editText_user_password);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 
